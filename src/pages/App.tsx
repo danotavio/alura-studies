f7d60a27c0e2d3f8143f60167ba3e78a931ac5a9
Jordan Otávio <danotavio@gmail.com>
2022-07-19 10:59:44 -0300

    some styles

---
+++ src/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Form from "../components/form/index";
+import List from "../components/list/index"
+import './style.scss';
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Form />
+      <List />
+    </div>
+  );
+}
+
+export default App;
